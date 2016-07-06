5888ef0b89198438a34b387d743f739b9aa1fda3
hannah gaskins <hannahgaskins@hannahs-MacBook-Pro.local>
2016-07-06 09:30:01 -0700

    timer big fix

--- HIITimer/StagingViewController.swift
+++ HIITimer/StagingViewController.swift
@@ -12,24 +12,21 @@ import UIKit
 class StagingViewController: UIViewController {
     
     // variables for timers
-    var timer = NSTimer()
-    var second = Int()
-    var fractions = Int()
+    var timer: NSTimer?
     var timerString: String = ""
     
     @IBOutlet weak var countDownTimer: UILabel!
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        printUserSettings() // for testing remove when finished
-        countDownTimer.text = "3" // on load 3 seconds displays
+//        countDownTimer.text = "3" // on load 3 seconds displays
         setCountDownLabel()
         
     }
     
     // ------------------ for testing remove when finished
+    
     func printUserSettings() {
         print(UserRoutine.shared.sets)
         print(UserRoutine.shared.activeTime)
@@ -43,22 +40,31 @@ class StagingViewController: UIViewController {
      */
     
     var initialTimerValue = 3
+    var setsCount = UserRoutine.shared.sets
+    var activeTimerValue = UserRoutine.shared.activeTime
+    var restTimerValue = UserRoutine.shared.restTime
     
     func update() {
         
         if(initialTimerValue > 0) {
+            initialTimerValue -= 1
             countDownTimer.text = "\(initialTimerValue)"
         }
         if initialTimerValue == 0 {
-            timer.invalidate()
-            startActiveTimer()
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.timer?.invalidate()
+                self.timer = nil
+                self.startActiveTimer()
+            })
+            
         }
-        initialTimerValue -= 1
         
+
     }
     
     func setCountDownLabel() {
         
+        countDownTimer.text = String(initialTimerValue)
         let blueColor = UIColor(red: 0/255.0, green: 0/255.0, blue: 255/255.0, alpha: 1.0)
         view.backgroundColor = blueColor
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
@@ -70,51 +76,83 @@ class StagingViewController: UIViewController {
      
      */
     
-    var activeTimerValue = UserRoutine.shared.activeTime
+    
+    
     
     func updateActiveTimer() {
         if(activeTimerValue > 0) {
+            activeTimerValue -= 1
             countDownTimer.text = "\(activeTimerValue)"
+            
         }
         
         if activeTimerValue == 0 {
-            timer.invalidate()
-            startRestTimer()
+            NSOperationQueue.mainQueue().addOperationWithBlock({ 
+                self.timer?.invalidate()
+                self.timer = nil
+                self.startRestTimer()
+                print(self.activeTimerValue)
+            })
         }
-        activeTimerValue -= 1
+        
     }
     
     
     func startActiveTimer() {
         
+        activeTimerValue = UserRoutine.shared.activeTime
         countDownTimer.text = String(activeTimerValue)
         let orangeColor = UIColor(red: 235/255.0, green: 102/255.0, blue: 40/255.0, alpha: 1.0)
         view.backgroundColor = orangeColor
         
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(self.updateActiveTimer), userInfo: nil, repeats: true)
+        
+       
 
     }
     
-    var restTimerValue = UserRoutine.shared.restTime
+    /*
+     
+     for rest timer
+     
+     */
+    
+    
     
     func updateRestTimer() {
         if restTimerValue > 0 {
+            restTimerValue -= 1
             countDownTimer.text = "\(restTimerValue)"
+            
         }
         if restTimerValue == 0 {
-            timer.invalidate()
-            startActiveTimer()
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.timer?.invalidate()
+                self.timer = nil
+                if self.setsCount > 0 {
+                    self.startActiveTimer()
+                } else if self.setsCount == 0 {
+                    print("🐔")
+                    // move to new congrats veiw
+                    
+                }
+            })
         }
-        restTimerValue -= 1
+        
+
+        
     }
     
     func startRestTimer() {
         
+        restTimerValue = UserRoutine.shared.restTime
         countDownTimer.text = String(restTimerValue)
         let restColor = UIColor(red: 63/255.0, green: 127/255.0, blue: 191/255.0, alpha: 1)
         view.backgroundColor = restColor
         
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(self.updateRestTimer), userInfo: nil, repeats: true)
+        
+         setsCount -= 1
     }
    
 }
